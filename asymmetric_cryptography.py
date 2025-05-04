348bdf8cf1b67611bd9b4325ed0551c6d7e60dc3
Rafael Matos <rafael.mmarinho@ufpe.br>
2025-05-04 08:13:18 -0400

    Add script python  asymmetric_cryptography.py

---
+++ asymmetric_cryptography.py
@@ -0,0 +1,63 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+
+
+def generate_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    public_key = private_key.public_key()
+    
+
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    
+    return private_key, public_key, private_pem, public_pem
+
+
+def encrypt_message(public_key, message):
+    encrypted = public_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted
+
+
+def decrypt_message(private_key, encrypted_message):
+    decrypted = private_key.decrypt(
+        encrypted_message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return decrypted.decode()
+
+
+private_key, public_key, private_pem, public_pem = generate_keys()
+
+
+message = "Olá, esta é uma mensagem secreta!"
+encrypted_message = encrypt_message(public_key, message)
+
+print("Mensagem criptografada:", encrypted_message)
+
+
+decrypted_message = decrypt_message(private_key, encrypted_message)
+
+print("Mensagem descriptografada:", decrypted_message)
